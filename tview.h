7e323cd98e40f0443a2174dc22f64f24c4dec024
golina123 <142676841+golina123@users.noreply.github.com>
2025-04-06 21:57:57 +0300

    Update tview.h

--- tview.h
+++ tview.h
@@ -1,19 +1,38 @@
 #pragma once
 
 #include "view.h"
+#include <cstdlib>
 
-enum colors {red = 1, green = 2, white = 7};
+enum colors {red = 1, green = 2, yellow = 3, blue = 4, pink = 5, cyan = 6, white = 7};
+
+struct fruit{
+        int x;
+        int y;
+};
+
+
 class tview : public view
 {
-	private:
-		int screen_x, screen_y;
-		void  clscr();
-		void cursor(int x, int y);
-	        void color(enum colors c);	
-		
-	public:
-		tview();
-		void ramka();
-		virtual void  draw();
+        private:
+                int screen_x, screen_y;
+                void  clscr();
+                void cursor(int x, int y);
+                void get_color(enum colors c);
+
+        public:
+                tview();
+                void horizontal_ramka(int x, int y);
+                void snake_body(int x, int y);
+                void vertical_ramka(int x, int y);
+                virtual void  draw();
+                void print_fruit(int n, int m);
+                struct apple{
+                              struct fruit a;
+                };
+
+                struct orange{
+                                struct fruit o;
+                };
+                fruit make_fruit(int widht, int height);
 
 };
