1257ca87cbb07c2c2b2504277b2cf5597e0df8d3
golina123 <142676841+golina123@users.noreply.github.com>
2025-04-06 14:34:32 +0300

    Update tview.h

--- tview.h
+++ tview.h
@@ -2,14 +2,18 @@
 
 #include "view.h"
 
+enum colors {red = 1, green = 2, white = 7};
 class tview : public view
 {
 	private:
+		int screen_x, screen_y;
 		void  clscr();
-		void cursor();
-	        void color(const char *str);	
+		void cursor(int x, int y);
+	        void color(enum colors c);	
+		
 	public:
+		tview();
+		void ramka();
 		virtual void  draw();
 
 };
-
