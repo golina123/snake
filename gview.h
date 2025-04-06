d8bebbca80989e9c81d195624fb2e3f6090bb180
golina123 <142676841+golina123@users.noreply.github.com>
2025-04-06 14:35:20 +0300

    Add files via upload

---
+++ gview.h
@@ -0,0 +1,12 @@
+//запоминает положение головы
+class snake{
+	private:
+		
+	public:
+		bool turn( sf::Keyboard::Key key );
+};
+    bool keyb<T>::turn (sf::Keyboard::Key key)
+    {
+        snake.turn(key);
+        return false;
+    }
